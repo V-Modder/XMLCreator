12b879ee61541d51381ee71ea2fad8601efe1cea
V-Modder <V-Modder@web.de>
2014-11-17 20:51:45 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace XMLCreator
+{
+    partial class XMLCreator
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_path = new System.Windows.Forms.Button();
+            this.txt_path = new System.Windows.Forms.TextBox();
+            this.btn_create = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txt_files = new System.Windows.Forms.TextBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.txt_url = new System.Windows.Forms.TextBox();
+            this.txt_description = new System.Windows.Forms.TextBox();
+            this.lbl_path = new System.Windows.Forms.Label();
+            this.lbl_files = new System.Windows.Forms.Label();
+            this.lbl_url = new System.Windows.Forms.Label();
+            this.lbl_description = new System.Windows.Forms.Label();
+            this.lbl_args = new System.Windows.Forms.Label();
+            this.txt_args = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_path
+            // 
+            this.btn_path.Location = new System.Drawing.Point(234, 25);
+            this.btn_path.Name = "btn_path";
+            this.btn_path.Size = new System.Drawing.Size(37, 23);
+            this.btn_path.TabIndex = 0;
+            this.btn_path.Text = "...";
+            this.btn_path.UseVisualStyleBackColor = true;
+            this.btn_path.Click += new System.EventHandler(this.btn_path_Click);
+            // 
+            // txt_path
+            // 
+            this.txt_path.Location = new System.Drawing.Point(12, 27);
+            this.txt_path.Name = "txt_path";
+            this.txt_path.Size = new System.Drawing.Size(216, 20);
+            this.txt_path.TabIndex = 1;
+            // 
+            // btn_create
+            // 
+            this.btn_create.Location = new System.Drawing.Point(94, 390);
+            this.btn_create.Name = "btn_create";
+            this.btn_create.Size = new System.Drawing.Size(75, 42);
+            this.btn_create.TabIndex = 2;
+            this.btn_create.Text = "Create Update.xml";
+            this.btn_create.UseVisualStyleBackColor = true;
+            this.btn_create.Click += new System.EventHandler(this.btn_create_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txt_files
+            // 
+            this.txt_files.Location = new System.Drawing.Point(11, 83);
+            this.txt_files.Multiline = true;
+            this.txt_files.Name = "txt_files";
+            this.txt_files.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txt_files.Size = new System.Drawing.Size(259, 119);
+            this.txt_files.TabIndex = 3;
+            this.txt_files.WordWrap = false;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(234, 54);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(36, 23);
+            this.btn_add.TabIndex = 4;
+            this.btn_add.Text = "+";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.Multiselect = true;
+            // 
+            // txt_url
+            // 
+            this.txt_url.Location = new System.Drawing.Point(12, 229);
+            this.txt_url.Name = "txt_url";
+            this.txt_url.Size = new System.Drawing.Size(259, 20);
+            this.txt_url.TabIndex = 5;
+            // 
+            // txt_description
+            // 
+            this.txt_description.Location = new System.Drawing.Point(12, 275);
+            this.txt_description.Multiline = true;
+            this.txt_description.Name = "txt_description";
+            this.txt_description.Size = new System.Drawing.Size(258, 66);
+            this.txt_description.TabIndex = 6;
+            // 
+            // lbl_path
+            // 
+            this.lbl_path.AutoSize = true;
+            this.lbl_path.Location = new System.Drawing.Point(12, 9);
+            this.lbl_path.Name = "lbl_path";
+            this.lbl_path.Size = new System.Drawing.Size(29, 13);
+            this.lbl_path.TabIndex = 7;
+            this.lbl_path.Text = "Pfad";
+            // 
+            // lbl_files
+            // 
+            this.lbl_files.AutoSize = true;
+            this.lbl_files.Location = new System.Drawing.Point(12, 64);
+            this.lbl_files.Name = "lbl_files";
+            this.lbl_files.Size = new System.Drawing.Size(84, 13);
+            this.lbl_files.TabIndex = 8;
+            this.lbl_files.Text = "Weitere Dateien";
+            // 
+            // lbl_url
+            // 
+            this.lbl_url.AutoSize = true;
+            this.lbl_url.Location = new System.Drawing.Point(12, 213);
+            this.lbl_url.Name = "lbl_url";
+            this.lbl_url.Size = new System.Drawing.Size(66, 13);
+            this.lbl_url.TabIndex = 9;
+            this.lbl_url.Text = "Upload URL";
+            // 
+            // lbl_description
+            // 
+            this.lbl_description.AutoSize = true;
+            this.lbl_description.Location = new System.Drawing.Point(12, 259);
+            this.lbl_description.Name = "lbl_description";
+            this.lbl_description.Size = new System.Drawing.Size(110, 13);
+            this.lbl_description.TabIndex = 10;
+            this.lbl_description.Text = "Update Beschreibung";
+            // 
+            // lbl_args
+            // 
+            this.lbl_args.AutoSize = true;
+            this.lbl_args.Location = new System.Drawing.Point(11, 348);
+            this.lbl_args.Name = "lbl_args";
+            this.lbl_args.Size = new System.Drawing.Size(76, 13);
+            this.lbl_args.TabIndex = 11;
+            this.lbl_args.Text = "Startparameter";
+            // 
+            // txt_args
+            // 
+            this.txt_args.Location = new System.Drawing.Point(12, 364);
+            this.txt_args.Name = "txt_args";
+            this.txt_args.Size = new System.Drawing.Size(258, 20);
+            this.txt_args.TabIndex = 12;
+            // 
+            // XMLCreator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 440);
+            this.Controls.Add(this.txt_args);
+            this.Controls.Add(this.lbl_args);
+            this.Controls.Add(this.lbl_description);
+            this.Controls.Add(this.lbl_url);
+            this.Controls.Add(this.lbl_files);
+            this.Controls.Add(this.lbl_path);
+            this.Controls.Add(this.txt_description);
+            this.Controls.Add(this.txt_url);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.txt_files);
+            this.Controls.Add(this.btn_create);
+            this.Controls.Add(this.txt_path);
+            this.Controls.Add(this.btn_path);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "XMLCreator";
+            this.Text = "XMLCreator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_path;
+        private System.Windows.Forms.TextBox txt_path;
+        private System.Windows.Forms.Button btn_create;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txt_files;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.TextBox txt_url;
+        private System.Windows.Forms.TextBox txt_description;
+        private System.Windows.Forms.Label lbl_path;
+        private System.Windows.Forms.Label lbl_files;
+        private System.Windows.Forms.Label lbl_url;
+        private System.Windows.Forms.Label lbl_description;
+        private System.Windows.Forms.Label lbl_args;
+        private System.Windows.Forms.TextBox txt_args;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XMLCreator
+{
+    public partial class XMLCreator : Form
+    {
+        public XMLCreator()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_path_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txt_path.Text = openFileDialog1.FileName;
+            }
+            openFileDialog1.FileName = "";
+        }
+
+        private void btn_create_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sUrl = txt_url.Text;
+                if (!sUrl.EndsWith("/"))
+                    sUrl += "/";
+                //Get the fileversion
+                FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(txt_path.Text);
+                
+                //Create a new XML-file
+                XmlDocument doc = new XmlDocument();
+                //Add XML-header
+                XmlNode docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+                doc.AppendChild(docNode);
+
+                //Add <sharpUpdate> note
+                XmlNode sharpUpdate = doc.CreateElement("sharpUpdate");
+                doc.AppendChild(sharpUpdate);
+
+                //Add <update AppId="?">
+                XmlNode update = doc.CreateElement("update");
+                XmlAttribute updateAttrib = doc.CreateAttribute("appId");
+                updateAttrib.Value = Path.GetFileName(fvi.FileName).Substring(0, Path.GetFileName(fvi.FileName).IndexOf(".exe"));
+                update.Attributes.Append(updateAttrib);
+                sharpUpdate.AppendChild(update);
+
+                //Add <version>
+                XmlNode updateInfo = doc.CreateElement("version");
+                updateInfo.AppendChild(doc.CreateTextNode(fvi.FileVersion));
+                update.AppendChild(updateInfo);
+
+                //Add <file> - for main Executeable
+                XmlNode file = doc.CreateElement("file");
+                XmlNode url = doc.CreateElement("url");
+                url.AppendChild(doc.CreateTextNode(sUrl + Path.GetFileName(fvi.FileName)));
+                file.AppendChild(url);
+                XmlNode filename = doc.CreateElement("filename");
+                filename.AppendChild(doc.CreateTextNode(Path.GetFileName(fvi.FileName)));
+                file.AppendChild(filename);
+                XmlNode md5 = doc.CreateElement("md5");
+                md5.AppendChild(doc.CreateTextNode(MakeHashString(MD5.Create().ComputeHash(new FileStream(txt_path.Text, FileMode.Open)))));
+                file.AppendChild(md5);
+                update.AppendChild(file);
+
+                //Add <file> - for each resource
+                string[] ss = txt_files.Text.Split('\n');
+                foreach (string s in ss)
+                {
+                    if (s != "")
+                    {
+                        string sFile = "";
+                        if (s.EndsWith("\r"))
+                            sFile = s.Remove(s.Length - 1);
+                        else
+                            sFile = s;
+                        XmlNode fileLoop = doc.CreateElement("file");
+                        XmlNode urlLoop = doc.CreateElement("url");
+                        urlLoop.AppendChild(doc.CreateTextNode(sUrl + Path.GetFileName(sFile)));
+                        fileLoop.AppendChild(urlLoop);
+                        XmlNode filenameLoop = doc.CreateElement("filename");
+                        filenameLoop.AppendChild(doc.CreateTextNode(Path.GetFileName(sFile)));
+                        fileLoop.AppendChild(filenameLoop);
+                        XmlNode md5Loop = doc.CreateElement("md5");
+                        md5Loop.AppendChild(doc.CreateTextNode(MakeHashString(MD5.Create().ComputeHash(new FileStream(sFile, FileMode.Open)))));
+                        fileLoop.AppendChild(md5Loop);
+                        update.AppendChild(fileLoop);
+                    }
+                }
+
+                //Add <description>
+                XmlNode description = doc.CreateElement("description");
+                description.AppendChild(doc.CreateTextNode(txt_description.Text));
+                update.AppendChild(description);
+
+                //Add <launchArgs>
+                XmlNode lauchArgs = doc.CreateElement("launchArgs");
+                lauchArgs.AppendChild(doc.CreateTextNode(txt_args.Text));
+                update.AppendChild(lauchArgs);
+
+                //Save XML-file to main executeable path 
+                doc.Save(new FileStream(Path.GetDirectoryName(txt_path.Text) + "\\update.xml", FileMode.Create));
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Vorgang abgeschlossen");
+            }
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string s in openFileDialog2.FileNames)
+                {
+                    txt_files.Text += s + Environment.NewLine;
+                }
+            }
+        }
+
+        private static string MakeHashString(byte[] hash)
+        {
+            StringBuilder s = new StringBuilder();
+
+            foreach (byte b in hash)
+                s.Append(b.ToString("x2").ToLower());
+
+            return s.ToString();
+        }
+    }
+}
